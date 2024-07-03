6dfec67d41b0c565b4d75802b8dc6904b71cba82
MesQue1041 <dovebookt@gmail.com>
2024-07-03 16:37:46 +0530

    Project Completed

--- client/src/Components/Addtask.js
+++ client/src/Components/Addtask.js
@@ -1,32 +1,31 @@
 import './Addtask.css'
 import axios from 'axios'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 function Addtask(props) {
-    const [task,Settask] = useState("")
-    const addtask = () => {
-        if(task.trim() === ''){
-            return
-        }
-        else {
-            axios.post('http://localhost:8000/api/tasks' , {
-                todo : task,
-                isComplete : false
-            }).then(res => {
-                Settask("")
-                props.addtask(res.data)
-            }).catch(err => console.log(err))
-        }
+  const [task, Settask] = useState("")
+
+  const addTask = () => {
+    if (task.trim() === '') {
+      return
+    } else {
+      axios.post('http://localhost:8000/api/tasks', {
+        todo: task,
+        isComplete: false
+      }).then(res => {
+        Settask("")
+        props.addTask(res.data) 
+      }).catch(err => console.log(err))
     }
-    return (
-        <div className='addtask'>
-            <input type='text' placeholder='Add Task . . .' value = {task} 
-            onChange={event => Settask(event.target.value)} />
-            <button onClick={() => addtask()}>Add Task</button>
-        </div>
-    )
+  }
 
+  return (
+    <div className='addtask'>
+      <input type='text' placeholder='Add Task . . .' value={task}
+        onChange={event => Settask(event.target.value)} />
+      <button onClick={addTask}>Add Task</button>
+    </div>
+  )
 }
-
 
 export default Addtask;
--- client/src/Components/Todolist.js
+++ client/src/Components/Todolist.js
@@ -1,33 +1,55 @@
-import './Todolist.css'
-import React from 'react'
+import './Todolist.css';
+import React from 'react';
 import CheckIcon from '@mui/icons-material/Check';
 import EditIcon from '@mui/icons-material/Edit';
 import CloseIcon from '@mui/icons-material/Close';
-
+import axios from 'axios';
 
 function Todolist(props) {
-    const todolist = props.todolist.map((task,index) => {
-        return <li key = {index}>
-            <div>
-                <CheckIcon className= {task.isComplete ? 'isComplete' : 'checkicon'}/>
-                <p className= {task.isComplete ? 'taskcomplete' : ''}>{task.todo}</p>
-            </div>
-            <div>
-                <EditIcon className='edit'/>
-                <CloseIcon className='close'/>
-            </div>
-        </li>
-    })
+    const todolist = props.todolist.map((task, index) => {
+        const taskComplete = task => {
+            axios.put(`http://localhost:8000/api/tasks/${task._id}`, {
+                _id: task._id,
+                todo: task.todo,
+                isComplete: !task.isComplete
+            })
+            .then(res => props.taskComplete(res.data))
+            .catch(err => console.log(err));
+        };
 
-  return (
-    <div className='tasklist'>
-        <ul>
-            {todolist}
-        </ul>
+        const removeTask = id => {
+            axios.delete(`http://localhost:8000/api/tasks/${id}`)
+                .then(res => {
+                    console.log("Deleted task:", res.data);
+                    props.removeTask({ _id: id });
+                })
+                .catch(err => console.log(err));
+        };
 
-    </div>
-    
-  )
+        return (
+            <li key={index}>
+                <div>
+                    <CheckIcon className={task.isComplete ? 'isComplete' : 'checkicon'} />
+                    <p className={task.isComplete ? 'taskcomplete' : ''} onClick={() => taskComplete(task)}>{task.todo}</p>
+                </div>
+                <div>
+                    <EditIcon className='edit' onClick={() => {
+                        props.tasktoUpdate(task);
+                        props.showPopup();
+                    }} />
+                    <CloseIcon className='close' onClick={() => removeTask(task._id)} />
+                </div>
+            </li>
+        );
+    });
+
+    return (
+        <div className='tasklist'>
+            <ul>
+                {todolist}
+            </ul>
+        </div>
+    );
 }
 
 export default Todolist;
